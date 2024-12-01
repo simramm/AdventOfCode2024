e7c3270e551616629509a185c82381b117de454d
Simon Ramm <simramm@gmail.com>
2024-12-01 06:17:02 +0100

    clean up

--- Dec01/Task_2.py
+++ Dec01/Task_2.py
@@ -3,19 +3,7 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 f = open(os.path.join(__location__,"Input.txt"))
 
-# if test needed
-#f = open(os.path.join(__location__,"Input_test.txt"))
-
-#data = f.read().split("\n")
-
-#data = f.read().split()
-#data = list(map(int, f.read().split('\n')))
-#data = list(map(int, f.read().split(',')))
 data = [x.split("   ") for x in f.read().split("\n")]
-
-print(data)
-
-
 
 left =[]
 right = []
@@ -24,14 +12,8 @@ for x in data:
     left.append(int(x[0]))
     right.append(int(x[1]))
     
-
-
 left.sort()
 right.sort()
-
-
-print(left)
-print(right)
 
 result=0
 for l in left:
