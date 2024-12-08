24a82e3abaf937771821a171ee8bd69abd25c2da
Simon Ramm <simramm@gmail.com>
2024-12-08 07:18:10 +0100

    dec08 done

--- 08.py
+++ 08.py
@@ -34,8 +34,8 @@ for a in antennas:
             newan2c=a[2]-diffc
             
             if 0<=newan2r<R and 0<=newan2c<C:
-                antinodesp1.add((newan2r,newan2c))
-
+                antinodesp1.add((newan2r,newan2c))       
+                           
 for a in antennas:    
     for b in antennas:        
         if a[0] == b[0] and not a == b:
@@ -43,22 +43,22 @@ for a in antennas:
             diffr = b[1]-a[1]
             diffc = b[2]-a[2]
             
-            newan1r=b[1]+diffr
-            newan1c=b[2]+diffc           
+            newan1r=b[1]
+            newan1c=b[2]           
             
             while 0<=newan1r<R and 0<=newan1c<C:                
                 antinodesp2.add((newan1r,newan1c))
                 newan1r+=diffr
                 newan1c+=diffc
                 
-            newan2r=a[1]+diffr
-            newan2c=a[2]+diffc
+            newan2r=a[1]
+            newan2c=a[2]
     
             while 0<=newan2r<R and 0<=newan2c<C:                
                 antinodesp2.add((newan2r,newan2c))
                 newan2r-=diffr
-                newan2c-=diffc            
-         
+                newan2c-=diffc  
+                                     
 print(len(antinodesp1))
 print(len(antinodesp2))
 
