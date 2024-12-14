07bac48d6e9a0b4c886b00c0e751d0a2219572f4
Simon Ramm <simramm@gmail.com>
2024-12-14 18:31:02 +0100

    dec12 p2 - took a while...

--- 12.py
+++ 12.py
@@ -1,13 +1,12 @@
 with open(__file__[-5:-3]+'.in') as f: data = f.read()
-#with open(__file__[-5:-3]+'-test.in') as f: data = f.read()
 
 lines = data.split("\n")
-
 R = len(lines)
 C = len(lines[0])
 
 areas = []
 done = set()
+
 for r in range(R):
     for c in range(C):
         area = set()
@@ -50,18 +49,35 @@ def perimeter(area):
         p+=4-n   
     return p
 
-def sides(area): 
+def corners(area): 
     s=0
-    #??????
+    for a in area:
+        c=0        
+        ar,ac = a
+        # outside
+        if (ar+1,ac) not in area and (ar,ac+1) not in area:
+            c+=1
+        if (ar+1,ac) not in area and (ar,ac-1) not in area:
+            c+=1
+        if (ar-1,ac) not in area and (ar,ac+1) not in area:
+            c+=1
+        if (ar-1,ac) not in area and (ar,ac-1) not in area:
+            c+=1
+        # inside
+        if (ar+1,ac) in area and (ar+1,ac+1) not in area and (ar,ac+1) in area:
+            c+=1
+        if (ar+1,ac) in area and (ar+1,ac-1) not in area and (ar,ac-1) in area:
+            c+=1
+        if (ar-1,ac) in area and (ar-1,ac+1) not in area and (ar,ac+1) in area:
+            c+=1
+        if (ar-1,ac) in area and (ar-1,ac-1) not in area and (ar,ac-1) in area:
+            c+=1        
+        s+=c
     return s
 
-p1=0
+p1,p2=0,0
 for area in areas:    
     p1+= len(area) * perimeter(area)
+    p2+= len(area) * corners(area)
 print(p1)
-
-
-p2=0
-for area in areas:    
-    p1+= len(area) * sides(area)
 print(p2)
